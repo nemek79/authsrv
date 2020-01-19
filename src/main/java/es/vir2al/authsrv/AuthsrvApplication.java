2ef20282a8ce351b9a9a4499cf62eac1e7acf0d4
DAVID ALONSO <d.alonso.sanchez@accenture.com>
2020-01-19 15:35:31 +0100

    Inicio servidor autorización

---
+++ src/main/java/es/vir2al/authsrv/AuthsrvApplication.java
@@ -0,0 +1,13 @@
+package es.vir2al.authsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthsrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/vir2al/authsrv/AuthsrvApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vir2al.authsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthsrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
