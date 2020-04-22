f8a50cbb6629e1e4d6748a5862e47382f0f88a68
David <davidalonso79@gmail.com>
2020-04-22 19:30:11 +0200

    mejoras instalacion

---
+++ bbdd/add_prestamos_project_rollback.sql
@@ -0,0 +1,8 @@
+use authbd;
+
+-- Prestamo$01P@sswd
+INSERT INTO t_aplicaciones VALUES
+(null,'PRESTAMOS','$2y$12$hxvqkmaGnWYsTZwZY1ghDOkY6gNvJahxXZPb4VFLw1k9aJppSqto2','PRES');
+
+DELETE FROM t_aplicaciones WHERE client_id = 'PRESTAMOS';
+
