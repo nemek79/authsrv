3ef640ba17e79cbac259728308fb2abf1b68bb5a
David <davidalonso79@gmail.com>
2020-04-23 18:48:01 +0200

    Mejoras instalación bbdd

---
+++ bbdd/install.sql
@@ -0,0 +1,2 @@
+source E:\WORKSPACES\FRAMEWORK\authsrv\bbdd\auth_bbdd.sql
+source E:\WORKSPACES\FRAMEWORK\authsrv\bbdd\add_prestamos_project.sql
---
+++ bbdd/rollback.sql
@@ -0,0 +1,2 @@
+source E:\WORKSPACES\FRAMEWORK\authsrv\bbdd\add_prestamos_project_rollback.sql
+source E:\WORKSPACES\FRAMEWORK\authsrv\bbdd\auth_bbdd_rollback.sql
